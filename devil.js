c0c51e3996d15b3da59c8b5999dc98224abeca87
frmnsyhxixi <93929002+frmnsyhxixi@users.noreply.github.com>
2022-01-19 05:26:17 +0700

    update devil.js

--- devil.js
+++ devil.js
@@ -1470,7 +1470,7 @@ case 'listpo':
 23 Jan. 
 1118297885 13498`)
              break
-case 'promo2':
+case 'mlpo':
                 gopeynya = 'https://i.ibb.co/hyVhcph/c5a5598a607a.jpg'
               teksnya = `- *PO SKIN GLOO SPECIAL*
 Harga = Rp 70.740
@@ -1898,7 +1898,7 @@ Order :`
         
         case 'list':
                list = []
-               listmenu = [`listpo`,`promo`,`promo2`,`coa`,`promosl`,`ff`,`cashpb`,`ml`,`sosis`,`cashpb`,`netflix`,`mlgift`,`genshin`,`genshinv`,`pubg`,`mld`,`mlsuperslow`,`sosmed`,`ragnarox`,`dragonraja`,`valorant`,`rekber`,`rdp`,`sewabott`,`caraorder`,`format1`,`pay`,`tf`,`reseller`,`owner`]
+               listmenu = [`listpo`,`promo`,`mlpo`,`coa`,`promosl`,`ff`,`cashpb`,`ml`,`sosis`,`cashpb`,`netflix`,`mlgift`,`genshin`,`genshinv`,`pubg`,`mld`,`mlsuperslow`,`sosmed`,`ragnarox`,`dragonraja`,`valorant`,`rekber`,`rdp`,`sewabott`,`caraorder`,`format1`,`pay`,`tf`,`reseller`,`owner`]
                listmenuu = [`List PO`,`PROMO ALL`,`MLBB PO`,`CRYSTAL AURORA`,`PROMO SL`,`LIST Free Fire`,`Cash PB`,`LIST MLBB`,`LIST Sausage Man`,`List Point Blank`,`Akun Netflix`,`Gift MLBB`,`List Genshin`,`List Genshin Vilog`,`List Pubg`,`Ml Paket D`,`Ml Super Slow`,`List Sosmed`,`List Ragnarox`,`List Dragon Raja`,`List Valorant`,`Harga Rekber`,`List RDP VPS`,`Harga BOT Wa`,`Cara Order`,`Format Order`,`Metode Pembayaran`,`BUKTI TF`,`JOIN RESELLER`,`OWNER`]
                nombor = 1
                startnum = 0
